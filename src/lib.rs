a1db86699c7bc3c847c9298a6bc561749f0d364a
Dipendra Singh <dipendras953@gmail.com>
2026-01-14 01:12:46 +0530

    init

---
+++ src/lib.rs
@@ -0,0 +1,16 @@
+use solana_program_entrypoint::entrypoint;
+use solana_account_info::AccountInfo;
+use solana_program_error::ProgramResult;
+use solana_pubkey::Pubkey;
+use solana_msg::msg;
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8]
+) -> ProgramResult{
+    msg!("CLMM MVP: Processing instruction");
+    //ToDo
+}
